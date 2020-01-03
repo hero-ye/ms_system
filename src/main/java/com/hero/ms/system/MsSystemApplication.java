82cd59d10871ae04ebb1f59122794dffb3c4a223
yejx <yejinxiong01@126.com>
2020-01-03 12:46:56 +0800

    添加跨域配置

--- src/main/java/com/hero/ms/system/MsSystemApplication.java
+++ src/main/java/com/hero/ms/system/MsSystemApplication.java
@@ -1,9 +1,12 @@
 package com.hero.ms.system;
 
+import com.hero.ms.common.filter.CorsOriginFilter;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
 
 @Log
 @EnableEurekaClient
@@ -13,6 +16,14 @@ public class MsSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsSystemApplication.class, args);
         log.info("已启动");
+    }
+
+    /**
+     * 解决跨域（方案二：全局配置）
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsOriginFilter().corsFilter();
     }
 
 }
